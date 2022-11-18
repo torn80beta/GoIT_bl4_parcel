3168045b5a251fdd6dc005fd813a8f2b798e436f
torn80beta <103459735+torn80beta@users.noreply.github.com>
2022-11-18 19:35:57 +0000

    Deploying to gh-pages from @ torn80beta/GoIT_bl4_parcel@ffebd7202f04af95a32f0d3854a2f2a211d7ca85 🚀

--- index.0892d451.js
+++
@@ -1,2 +0,0 @@
-$(".variable-width").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,centerMode:!0,variableWidth:!0});
-//# sourceMappingURL=index.0892d451.js.map
--- index.6962102c.js
+++
@@ -1,2 +0,0 @@
-$(".variable-width").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,centerMode:!0,variableWidth:!0});
-//# sourceMappingURL=index.6962102c.js.map
